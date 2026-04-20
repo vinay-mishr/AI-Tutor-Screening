c7930bb31cd2da871acdc6a5682527b7ec2cafea
Rudra Singh <rudrasingh@Rudras-MacBook-Air.local>
2026-04-20 10:40:56 +0530

    minor tweking

--- server/index.js
+++ server/index.js
@@ -121,11 +121,13 @@ app.post("/api/interview", aiLimiter, async (req, res) => {
 
 const path = require("path");
 
+const buildPath = path.join(process.cwd(), "client/build");
+
 if (process.env.NODE_ENV === "production") {
-  app.use(express.static(path.join(__dirname, "../client/build")));
+  app.use(express.static(buildPath));
 
   app.get("*", (req, res) => {
-    res.sendFile(path.join(__dirname, "../client/build/index.html"));
+    res.sendFile(path.join(buildPath, "index.html"));
   });
 }
 
